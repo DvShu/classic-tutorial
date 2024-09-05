8766b0ec20b816d75956a2b782b74599f2d5d161
Tenny <joel.shu@qq.com>
2024-09-05 23:50:28 +0800

    feat: 读取联系人
    Signed-off-by: Tenny <joel.shu@qq.com>

--- docs/libs/contacts.ts
+++ docs/libs/contacts.ts
@@ -10,15 +10,53 @@ export function isSupport() {
   return "contacts" in navigator;
 }
 
-export async function getContactsData() {
-  if (isSupport()) {
-    try {
-      // 指定读取哪些信息
-      const props = ["tel", "name", "email", "icon", "address"];
-      // @ts-ignore
-      const contacts = await navigator.contacts.select(props);
-    } catch (error) {
-      console.log(error);
-    }
+interface ContactItem {
+  /** 联系人地址 */
+  address?: string[];
+  /** 邮件地址 */
+  email?: string[];
+  /** 图标 */
+  icon?: string[];
+  /** 联系人姓名 */
+  name?: string[];
+  /** 联系电话 */
+  tel?: string[];
+}
+
+/**
+ * 导出一个异步函数，用于获取联系人数据
+ *
+ * @param propertys 需要获取的联系人属性，默认为 ["tel", "name", "email", "icon", "address"]
+ * @param multiple 是否允许选择多个联系人，默认为 false
+ *
+ * @returns 返回一个 Promise，其中包含联系人数据
+ * @throws 如果当前浏览器不支持联系人功能，抛出一个错误
+ * @throws 如果发生其他错误，抛出一个错误
+ */
+export async function getContactsData(
+  propertys: (keyof ContactItem)[] = [
+    "tel",
+    "name",
+    "email",
+    "icon",
+    "address",
+  ],
+  multiple: false
+) {
+  // 判断是否支持联系人功能
+  if (!isSupport()) {
+    throw new Error("-1:当前浏览器不支持联系人功能");
+  }
+
+  try {
+    // @ts-ignore: 假设 navigator.contacts.select 是浏览器特定的 API，忽略类型检查
+    const contacts: ContactItem[] = await navigator.contacts.select(propertys, {
+      multiple,
+    });
+
+    return contacts;
+  } catch (error) {
+    // 捕获任何错误并抛出具有适当信息的错误
+    throw new Error("2:" + ((error as Error).message || "未知错误"));
   }
 }
--- docs/tutorial_list/phone_contacts.md
+++ docs/tutorial_list/phone_contacts.md
@@ -6,14 +6,18 @@
 
 <script setup>
   import '../styles/phone_contacts.css'
-  import { isSupport } from '../libs/contacts'
+  import { getContactsData } from '../libs/contacts'
   import { ref } from 'vue'
  
-  const errMsg = ref('')
+  const errMsg = ref('');
+  const contacts = ref('');
 
-  function handleGetContacts() {
-    if (!isSupport()) {
-      errMsg.value = '当前设备不支持读取联系人'
+  async function handleGetContacts() {
+    try {
+      const data = await getContactsData();
+      contacts.value = JSON.stringify(data, null, 2);
+    } catch (error) {
+      errMsg.value = error.message;
     }
   }
 </script>
@@ -24,4 +28,25 @@
 
 <div>
   <span v-if="errMsg !== ''" class="color-red">{{ errMsg }}</span>
+  <div>{{ contacts }}</div>
 </div>
+
+## 代码
+
+### 简单说明
+
+> 1. 联系人 `api` 运行在安全模式(`https`)下
+> 2. 通过 `"contacts" in navigator` 判断浏览器是否支持该特性
+> 3. `await navigator.contacts.select()` 打开联系人选择器，返回选中列表
+
+```javascript
+function isSupport() {
+  return "contacts" in navigator;
+}
+
+const contacts: ContactItem[] = await navigator.contacts.select(propertys, {
+  multiple,
+});
+```
+
+> 对于源码, 请参考 [classic-tutorial](https://github.com/DvShu/classic-tutorial) 工程目录下: `docs/libs/contacts.ts`
